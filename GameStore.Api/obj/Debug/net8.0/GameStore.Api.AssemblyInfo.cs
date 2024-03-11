a8e92579a1a0dfbb8795f0b2adcb3bd7d7372a94
Yonathan Manurung <yonrungrung@gmail.com>
2024-03-11 22:48:59 +0700

    in memory,handling not found resource for crud endpoint

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01643638d88853701841d235500371977b270008")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2dfaae36410054152d52e1b125e01133953c02")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
